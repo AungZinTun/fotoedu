12d62cb622cfffcff70bd0c0db696c334c1b7ff1
AungZinTun <42179343+AungZinTun@users.noreply.github.com>
2019-10-18 00:49:28 +0630

    db

--- FotoEdu/FotoEdu/settings.py
+++ FotoEdu/FotoEdu/settings.py
@@ -76,11 +76,22 @@ WSGI_APPLICATION = 'FotoEdu.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
+if DEBUG==False:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fotoedu',
+        'USER': 'root',
+        'PASSWORD': 'adin@1234',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
